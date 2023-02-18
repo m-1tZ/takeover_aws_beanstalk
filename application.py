def301b8d2d36f2118a3db08cd2af9e9b2d790ae
Moritz Oehrlein <moritz.oehrlein@gmail.com>
2023-02-18 21:35:58 +0100

    up2date

--- application.py
+++ application.py
@@ -1,65 +1,32 @@
-import logging.handlers
+from flask import Flask
 
-# Create logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
 
-# Handler 
-LOG_FILE = '/tmp/sample-app.log'
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
-handler.setLevel(logging.INFO)
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>POC takeover</title> </head>\n<body>'''
+instructions = '''
+    WW91ciBwYWdlIGJlbG9uZ3MgdG8gbTF0Wg==\n''' #Your page belongs to m1tZ
+home_link = ''
+footer_text = '</body>\n</html>'
 
-# Formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
-# Add Formatter to Handler
-handler.setFormatter(formatter)
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
 
-# add Handler to Logger
-logger.addHandler(handler)
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
 
-welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
-
-        http://aws.Amazon/apache2.0/
-
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>POC takeover AWS Beanstalk</title>
-</head>
-<body id="sample">
-  WW91ciBwYWdlIGJlbG9uZ3MgdG8gbTF0Wg==
-</body>
-</html>
-"""
-
-
-def application(environ, start_response):
-    path = environ['PATH_INFO']
-    method = environ['REQUEST_METHOD']
-    if method == 'POST':
-        try:
-            if path == '/':
-                request_body_size = int(environ['CONTENT_LENGTH'])
-                request_body = environ['wsgi.input'].read(request_body_size)
-                logger.info("Received message: %s" % request_body)
-            elif path == '/scheduled':
-                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'],
-                            environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
-        except (TypeError, ValueError):
-            logger.warning('Error retrieving request body for async work.')
-        response = ''
-    else:
-        response = welcome
-    start_response("200 OK", [
-        ("Content-Type", "text/html"),
-        ("Content-Length", str(len(response)))
-    ])
-    return [bytes(response, 'utf-8')]
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
